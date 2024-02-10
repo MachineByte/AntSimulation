8a34248fbb94d6d3455db2743dfce7eb7fd8db0e
Михаил Белянов <mihail.belyanov@gmail.com>
2024-02-10 15:20:58 +0700

    test commit

--- src/main/java/application/startproject/Main.java
+++ src/main/java/application/startproject/Main.java
@@ -2,22 +2,23 @@ package application.startproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        primaryStage.setTitle("Simple JavaFX App");
+        primaryStage.setScene(new Scene(root, 400, 200));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/application/startproject/SampleController.java
+++ src/main/java/application/startproject/SampleController.java
@@ -1,2 +1,16 @@
-package application.startproject;public class SampleController {
+package application.startproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SampleController {
+
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    public void handleSubmitButtonAction() {
+        String inputText = inputField.getText();
+        System.out.println("Input text: " + inputText);
+    }
 }
