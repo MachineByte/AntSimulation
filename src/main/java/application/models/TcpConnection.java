80f1c628c3643d683147752b3f8ec00bb6cdedd6
Михаил Белянов <mihail.belyanov@gmail.com>
2024-03-28 12:33:22 +0700

    Добавлены 2 потока с багами

--- src/main/java/application/models/TcpConnection.java
+++ src/main/java/application/models/TcpConnection.java
@@ -1,55 +1,89 @@
 package application.models;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TcpConnection {
     private static volatile TcpConnection instance;
     private Socket socket;
     private PrintWriter out;
-    private BufferedReader in;
-    private final Thread listenerThread;
-    private volatile boolean running = true;
+    private ObjectInputStream in;
+    private Thread listenerThread;
+
+    private final Thread senderThread;
+    private volatile boolean listenerRunning = true;
+    private volatile boolean senderRunning = true;
     private final String serverAddress = "127.0.0.1"; // Адрес сервера
     private final int port = 12345; // Порт сервера
+    private Set<String> connectionsSet = new HashSet<>();
+    AtomicInteger connectionAttemptCounter = new AtomicInteger();
+
     private TcpConnection() {
-        listenerThread = new Thread(() -> {
-            while (running) {
+        senderThread = new Thread(() -> {
+            // Цикл для чтения входящих сообщений
+            while (senderRunning) {
                 try {
-                    if (socket == null || socket.isClosed()) {
-                        // Попытка подключения к серверу
-                        connectToServer();
+                    if (socket == null || socket.isClosed() || connectionAttemptCounter.get() > 5) {
+                        closeResources();
+                        connectToServer(); // Попытка подключения к серверу
+                        connectionAttemptCounter.set(0);
                     }
 
-                    // Цикл для чтения входящих сообщений
-                    if (in.ready()) {
-                        String receivedMessage = in.readLine();
-                        System.out.println("Получено сообщение: " + receivedMessage);
-                        // Обработка полученного сообщения
-                    } else {
-                        // Небольшая задержка, чтобы избежать чрезмерного использования CPU
-                        Thread.sleep(1000);
-                    }
+                    out.println("1");
+                    connectionAttemptCounter.getAndIncrement();
+                    Thread.sleep(1000); // Небольшая задержка, чтобы избежать чрезмерного использования CPU
+//                    System.out.println(connectionAttemptCounter+"!");
+
                 } catch (IOException e) {
                     System.out.println("Ошибка соединения. Попытка переподключения...");
-                    closeResources(); // Закрытие текущего соединения
+                    try {
+                        closeResources(); // Закрытие текущего соединения
+                        Thread.sleep(500);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
                 } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    System.out.println("Поток прерван.");
+                    throw new RuntimeException(e);
                 }
             }
-
-            closeResources(); // Закрытие ресурсов при завершении работы
+            try {
+                closeResources(); // Закрытие ресурсов при завершении работы
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         });
-        listenerThread.start();
+
+        senderThread.start();
     }
 
     private void connectToServer() throws IOException {
         socket = new Socket(serverAddress, port);
         out = new PrintWriter(socket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        in = new ObjectInputStream(socket.getInputStream());
         System.out.println("Успешное подключение к серверу.");
+        listenerThread = new Thread(() -> {
+            while(listenerRunning){
+                Object obj = null;
+                try {
+                    obj = in.readObject();
+                    connectionsSet.addAll(((Set<String>) obj));
+                    System.out.println("Получено сообщение: " + connectionsSet + " " + System.currentTimeMillis());
+                    connectionAttemptCounter.set(0);
+                } catch (IOException | ClassNotFoundException e) {
+                    System.out.println("Ошибка при чтении");
+                    listenerRunning =  false;
+                }
+            }
+        });
+        listenerThread.start();
+        listenerThread.interrupt();
     }
+
 
     public static TcpConnection getInstance() {
         if (instance == null) {
@@ -62,17 +96,8 @@ public class TcpConnection {
         return instance;
     }
 
-    public void sendMessage(String msg) {
-        out.println(msg);
-    }
 
-    public void closeConnection() {
-        running = false;
-        listenerThread.interrupt();
-        closeResources();
-    }
-
-    private void closeResources() {
+    private void closeResources() throws InterruptedException {
         try {
             if (in != null) in.close();
             if (out != null) out.close();
@@ -83,5 +108,14 @@ public class TcpConnection {
         socket = null;
         out = null;
         in = null;
+    }
+
+    public String getIp() {
+        try {
+            return socket.getLocalSocketAddress().toString();
+        } catch (NullPointerException e) {
+            return null;
+        }
+
     }
 }
